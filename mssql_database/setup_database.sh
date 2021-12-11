8595f6e140644df8be4d75f8b722e52ad31cf621
DanielDu <danghoangnhan.1@gmail.com>
2021-12-11 03:45:02 +0000

    fix unresolve

--- mssql_database/setup_database.sh
+++ mssql_database/setup_database.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Wait for database to startup
 sleep 20
-./opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P <YOUR PASSWORD> -i setup.sql
+./opt/mssql-tools/bin/sqlcmd -S localhost -U SA -d testDB -P Test123@ -i test.sql
